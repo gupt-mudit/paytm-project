fcb765b3f7a2e751330ff78fe3bcf03bf93a24b9
gupt-mudit <muditgupt01@gmail.com>
2024-05-04 15:52:23 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://gupta_mudit:Starboy%4001@cluster0.tyihsd6.mongodb.net/')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+};
